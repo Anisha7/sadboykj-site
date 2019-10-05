4f4d45128536df8eedae4a6b895b2a17e80a2740
Anisha Jain <anishajain@outlook.com>
2019-10-05 02:58:25 -0700

    Form input validation

--- client/src/components/PurchaseTicketForm/index.js
+++ client/src/components/PurchaseTicketForm/index.js
@@ -5,14 +5,56 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './styles.css'
 
+// helpers for data validation
+function validateFirstName(val, ref) {
+    if (val.length === 0){
+        ref.className = 'invalidInput'
+        return false
+    }
+    ref.className = ''
+    return true
+}
+
+function validateLastName(val, ref) {
+    if (val.length === 0){
+        ref.className = 'invalidInput'
+        return false
+    }
+    ref.className = ''
+    return true
+}
+
+function validateEmail(val, ref) {
+    // eslint-disable-next-line
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const valid = re.test(String(val).toLowerCase());
+    if (!valid) {
+        ref.className = 'invalidInput'
+        return valid
+    }
+    ref.className = ''
+    return valid
+}
+
+function validateAge(val, ref) {
+    if (val < 1 || val > 90) {
+        ref.className = 'invalidInput'
+        return false
+    }
+    ref.className = ''
+    return true
+}
+
 class PurchaseTicketForm extends Component {
     constructor(props) {
         super(props)
         this.state={
             goHome: false,
-            name: '',
+            firstName: '',
+            lastName: '',
             email: '',
             age: 0,
+            error: false,
         }
     }
 
@@ -21,7 +63,13 @@ class PurchaseTicketForm extends Component {
     }
 
     validateData() {
-
+        const { firstName, lastName, email, age } = this.state
+        const result = validateFirstName(firstName, this.refs.firstName) & 
+                validateLastName(lastName, this.refs.lastName) &
+                validateAge(age, this.refs.age) &
+                validateEmail(email, this.refs.email)
+        this.setState({ error : !result})
+        return result
     }
 
     render() {
@@ -29,28 +77,49 @@ class PurchaseTicketForm extends Component {
             return <Redirect to="/" />
         }
 
-        // name
-        // email
-        // age (send int to backend) --> 1-90
+        let error = <p></p>
+        if (this.state.error) {
+            error = <p class="error">Please fix the highlighted input fields!</p>
+        }
+        console.log(error)
+        
         return (
             <HomeWrapper>
                 <FontAwesomeIcon onClick={()=>this.goHome()} className="backIcon" icon={faArrowLeft} size="2x" />
-                <div className="ticketForm">
-                    <input  name="fullname" 
-                            placeholder="Full name" 
-                            value={this.state.name} 
-                            onChange={(e) => this.setState({ name : e.target.value})} />
-                    <input  name="email" 
+                <div className="flex eventName">
+                    <h1>SAD BOY SHOWOUT</h1>
+                    <h2>HALLOWEEN</h2>
+                </div>
+                <div className="flex">
+                    <input  ref="firstName"
+                            name="firstName" 
+                            placeholder="First name" 
+                            value={this.state.firstName} 
+                            onChange={(e) => this.setState({ firstName : e.target.value})}
+                            required />
+                    <input  ref="lastName"
+                            name="lastName" 
+                            placeholder="Last name" 
+                            value={this.state.lastName} 
+                            onChange={(e) => this.setState({ lastName : e.target.value})} 
+                            required />
+                    <input  ref="email"
+                            name="email" 
                             placeholder="Email" 
                             type="email" 
                             value={this.state.email} 
-                            onChange={(e) => this.setState({ name : e.target.email})} />
-                    <input  name="age" 
+                            onChange={(e) => this.setState({ email : e.target.value})}
+                            required />
+                    <input  ref="age"
+                            name="age" 
                             placeholder="Age" 
                             type="number" value={this.state.age} 
-                            onChange={(e) => this.setState({ age : e.target.value})} />
-                    {/* <input type="submit" value="Get Free Ticket" onSubmit={() => console.log("HELLO")} /> */}
-                    <button className="submitButton" onClick={() => console.log("CLICKED")}> GET FREE TICKET </button>
+                            onChange={(e) => this.setState({ age : e.target.value})}
+                            required />
+                    <div>
+                        <button className="submitButton" onClick={() => this.validateData()}> GET FREE TICKET </button>
+                        {error}
+                    </div>
                 </div>
             </HomeWrapper>
         )
