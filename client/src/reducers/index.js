5b44a06e2e5fb7ee8f06f55f81fd5bc2cd55e12d
Anisha Jain <anishajain@outlook.com>
2019-10-05 05:14:52 -0700

    react redux implemented

---
+++ client/src/reducers/index.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux'
+import { TICKET_SUBMISSION_SUCCESS } from '../actions'
+
+const ticketSubmissionReducer = (state = [], action) => {
+    switch(action.type) {
+      case TICKET_SUBMISSION_SUCCESS:
+        const { success } = action.payload
+        return [...state, { success }]
+  
+      default:
+        return state
+    }
+};
+
+export default combineReducers({
+    ticketSubmissionSuccess: ticketSubmissionReducer
+})
