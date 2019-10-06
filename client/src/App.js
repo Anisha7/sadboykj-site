830ec2341473cc13caa52f5b95567150ba572fc5
Anisha Jain <anishajain@outlook.com>
2019-10-05 17:41:47 -0700

    Testing API using testAPI

--- client/src/App.js
+++ client/src/App.js
@@ -14,11 +14,26 @@ import reducers from './reducers';
 const store = createStore(reducers)
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
   render() {
     return (
       <Provider store={store}>
         <div className="wrapper">
+          <p className="App-intro">;{this.state.apiResponse}</p>
           <Router>
             <Switch>
                 <Route exact path="/">
@@ -44,29 +59,3 @@ class App extends Component {
 }
 
 export default App;
-// constructor(props) {
-  //   super(props);
-  //   this.state = { apiResponse: "" };
-  // }
-
-  // callAPI() {
-  //     fetch("http://localhost:9000/testAPI")
-  //         .then(res => res.text())
-  //         .then(res => this.setState({ apiResponse: res }));
-  // }
-
-  // componentWillMount() {
-  //     this.callAPI();
-  // }
-
-  // render() {
-  //   return (
-  //     <div className="App">
-  //       <div className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <h2>Welcome to React</h2>
-  //       </div>
-  //       <p className="App-intro">;{this.state.apiResponse}</p>
-  //     </div>
-  //   );
-  // }
