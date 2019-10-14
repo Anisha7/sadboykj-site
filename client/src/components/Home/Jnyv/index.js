66cff5fc242753eb8d00592c4acd90db531a1533
Anisha Jain <anishajain@outlook.com>
2019-10-14 15:30:14 -0700

    jnyv bug fixes

--- client/src/components/Home/Jnyv/index.js
+++ client/src/components/Home/Jnyv/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from './node_modules/react';
-import { Redirect } from './node_modules/react-router-dom'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 import '../../../commonStyles.css'
 import '../styles.css';
-import cover from '../../../assets/forghetto.png';
+import cover from '../../../assets/jnyv.png';
 import HomeWrapper from '../../HomeWrapper'
 
 class JnyVHome extends Component {
@@ -18,10 +18,10 @@ class JnyVHome extends Component {
             return <Redirect to='/tickets' />
         }
         return (
-        <HomeWrapper soundcloudLink="https://soundcloud.com/510zona" 
-                     itunesLink="https://music.apple.com/us/artist/zonaccino/1420222299"
-                     instagramLink="https://www.instagram.com/zonaccino" 
-                     spotifyLink="https://open.spotify.com/artist/6u7zrSHrCMvA9obfrFrLDj?si=StlWyJUoRnyL19-Slps74g">
+        <HomeWrapper soundcloudLink="https://soundcloud.com/officialjny_v" 
+                     itunesLink="https://music.apple.com/us/artist/jny-v/1346442082"
+                     instagramLink="http://Instagram.com/jny_v" 
+                     spotifyLink="https://open.spotify.com/artist/07k8QTozWJl9I8DUW1nKld?si=42u-RPQjQBWwiBm65_yVOQ">
             <img src={cover} className="cover" alt="cover" onClick={() => this.setState({ redirectToTicketForm : true })} />
         </HomeWrapper>)
     }
